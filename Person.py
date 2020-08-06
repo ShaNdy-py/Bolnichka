97227cdd32aaea0e7aefe031c5b5ef60c7981338
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2020-08-06 13:59:42 +0400

    Add files via upload

---
+++ Person.py
@@ -0,0 +1,8 @@
+class Person(): # ага - да
+	
+	def __init__(self, name, family, patron, number):
+		self.name = name
+		self.family_name = family
+		self.patronymic = patron
+		self.phone_number = number
+
