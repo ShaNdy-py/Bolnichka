1ed782131ecaba1e3f8077b84f23c25a8d44c5f8
ShaNdy-py <69586575+ShaNdy-py@users.noreply.github.com>
2020-08-12 17:49:33 +0400

    Create Space.py

---
+++ Space.py
@@ -0,0 +1,18 @@
+class space():
+    
+    def __init__(self, free, person):
+        self.free = free
+        self.person = person
+
+    
+    def set_free(self, name):
+        self.name = name
+
+    def set_person(self, person):
+        self.person = person
+
+    def get_free(self):
+        return self.free
+
+    def get_person(self):
+        return self.person
