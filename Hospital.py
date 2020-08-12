497a5eb91491b1633e0eda5aaf7e27412c010eba
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2020-08-12 17:49:10 +0400

    Update Hospital.py

--- Hospital.py
+++ Hospital.py
@@ -1,3 +1,15 @@
 class Hospital():
 	
-	def __init__(self, arg):
+	NAMES_OF_DEPARTMENTS = []
+
+	def __init__(self, wards):
+		self.list_of_wards = wards
+
+	def set_list_of_wards(self, wards):
+		self.list_of_wards = wards
+
+	def get_list_of_wards(self):
+		return self.list_of_wards
+
+	def check_free_wards(self):
+	 	
